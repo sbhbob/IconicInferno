f4d4e10f456d111bbf8fc68c2d8b1cfe84eef0dd
Skyler Hope <99628593+sbhbob@users.noreply.github.com>
2023-05-26 19:47:57 -0600

    Second commit

---
+++ IconicInferno/IconicInfernoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconicInfernoApp.swift
+//  IconicInferno
+//
+//  Created by Skyler Hope on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct IconicInfernoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
